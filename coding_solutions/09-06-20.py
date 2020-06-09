80ffb308d4e3456a8167dd4f35095abd479f7a56
krishna katira <krishnakatira98@gmail.com>
2020-06-09 12:42:43 +0530

    09-06-20

---
+++ coding_solutions/09-06-20.py
@@ -0,0 +1,11 @@
+string=raw_input("Enter string:")
+count1=0
+count2=0
+for i in string:
+      if(i.isdigit()):
+            count1=count1+1
+      count2=count2+1
+print("The number of digits is:")
+print(count1)
+print("The number of characters is:")
+print(count2)
