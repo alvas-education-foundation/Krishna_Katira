ca2059df5d90ee3f6f54d305bd566abb7c59c016
krishna katira <krishnakatira98@gmail.com>
2020-06-12 12:01:32 +0530

    12-06-20

---
+++ coding_solutions/12-06-20.py
@@ -0,0 +1,9 @@
+def modify(string):  
+  final = ""   
+  for i in range(len(string)):  
+    if i % 2 == 0:  
+      final = final + string[i]  
+  return final
+string=raw_input("Enter string:")
+print("Modified string is:")
+print(modify(string))
