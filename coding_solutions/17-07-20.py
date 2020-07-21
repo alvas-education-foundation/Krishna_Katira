547c2501632f1824718abe91de45d461c532027e
krishna katira <krishnakatira98@gmail.com>
2020-07-21 14:26:14 +0530

    17-07-20

---
+++ coding_solutions/17-07-20.py
@@ -0,0 +1,41 @@
+def select(alist, start, end, i):
+    """Find ith smallest element in alist[start... end-1]."""
+    if end - start <= 1:
+        return alist[start]
+    pivot = partition(alist, start, end)
+ 
+    # number of elements in alist[start... pivot]
+    k = pivot - start + 1
+ 
+    if i < k:
+        return select(alist, start, pivot, i)
+    elif i > k:
+        return select(alist, pivot + 1, end, i - k)
+ 
+    return alist[pivot]
+ 
+def partition(alist, start, end):
+    pivot = alist[start]
+    i = start + 1
+    j = end - 1
+ 
+    while True:
+        while (i <= j and alist[i] <= pivot):
+            i = i + 1
+        while (i <= j and alist[j] >= pivot):
+            j = j - 1
+ 
+        if i <= j:
+            alist[i], alist[j] = alist[j], alist[i]
+        else:
+            alist[start], alist[j] = alist[j], alist[start]
+            return j
+ 
+ 
+alist = input('Enter the list of numbers: ')
+alist = alist.split()
+alist = [int(x) for x in alist]
+i = int(input('The ith smallest element will be found. Enter i: '))
+ 
+ith_smallest_item = select(alist, 0, len(alist), i)
+print('Result: {}.'.format(ith_smallest_item))
